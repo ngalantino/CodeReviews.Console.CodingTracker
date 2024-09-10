0f02c68378ca076cd1c8abb7e9adef7cb0ae7d11
Nicolas Galantino <nick.galantino@gmail.com>
2024-09-10 07:48:10 -0400

    Create CodingSession model class.

---
+++ CodingTracker.ngalantino/CodingSession.cs
@@ -0,0 +1,11 @@
+
+public class CodingSession {
+    public int Id {get; set;}
+
+    public DateTime startTime {get; set;}
+
+    public DateTime endTime {get; set;}
+
+    public TimeSpan timeSpan {get; set;}
+
+}
